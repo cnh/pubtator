83ef3bedccde2efff58934e28036a14cf9a0ad10
cnh <ashish.makani@gmail.com>
2017-08-30 15:27:47 +0530

    Update check_gene_in_pmid.py

--- check_gene_in_pmid.py
+++ check_gene_in_pmid.py
@@ -1,11 +1,13 @@
 import requests
 import re
+import traceback
+import sys
+import logging
+
+PUBTATOR_ENDPOINT_URL = "https://www.ncbi.nlm.nih.gov/CBBresearch/Lu/Demo/RESTful/tmTool.cgi/Gene/"
 
 
-PUBTATOR_ENDPOINT = "https://www.ncbi.nlm.nih.gov/CBBresearch/Lu/Demo/RESTful/tmTool.cgi/Gene/"
-
-
-final_api_string = PUBTATOR_ENDPOINT + pmid + "/BioC"
+final_api_string = PUBTATOR_ENDPOINT_URL + pmid + "/BioC"
 
 #call the pubtator api endpoint
 pubtator_response = requests.get(final_api_string)
@@ -14,7 +16,18 @@ pubtator_response = requests.get(final_api_string)
 print(pubtator_response.url)
 
 #print raw response and json
-print(pubtator_response, pubtator_response.json())
+try:
+	print(pubtator_response, pubtator_response.json())
+except ValueError as ve:
+	logging.exception("Something awful happened!")
+	logger.error('json decoding failed, ValueError', exc_info=True)
+	print(traceback.format_exception(None, # <- type(e) by docs, but ignored 
+                                     ve, ve.__traceback__),
+          file=sys.stderr, flush=True)
+	print("json decoding failed : ", ve.p)
+	traceback.print_exc()
+	desired_trace = traceback.format_exc(sys.exc_info())
+    #log(desired_trace)
 
 #print response text
 print(pubtator_response.text)
